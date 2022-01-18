f0209dd159caf3e7678f95c03a1a7cbb19f6b039
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2022-01-18 15:39:57 +0530

    18-01-2022-vamshi

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const res = require('express/lib/response');
+const router = express.Router();
+
+var app=express();
+var bodyParser = require('body-parser')
+app.use( bodyParser.urlencoded() ); 
+
+router.get('/',(req,res)=>{
+    res.render('home');
+})
+
+router.get('/adminlogin',(req,res)=>{
+    res.render('adminlogin');
+})
+
+router.get('/student',(req,res)=>{
+    res.render('student');
+})
+router.post('/adminlogin',(Req, Res)=> {
+    var username = Req.body.name;
+    var password = Req.body.password;
+
+    if (username=='admin' && password == 'vamshi') {
+           // do something here with a valid login
+          
+            Res.render('adminhome');
+    } else { 
+        Res.redirect('adminlogin')
+           // user or password doesn't match
+    }
+});
+module.exports = router;
